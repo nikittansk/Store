a1020a6f2b54a30c90f5b29fb1dcb9d16f7ab54b
Евгений Гавришин <geugene1998@ya.ru>
2020-05-10 13:39:28 +0300

    Добавили логику "Корзина"

--- Программа для продаж.py
+++ Программа для продаж.py
@@ -6,6 +6,10 @@ class Product:
         self.__stock = stock
 
     @property
+    def id(self):
+        return self.__id
+
+    @property
     def price(self):
         return self.__price
 
@@ -29,20 +33,80 @@ class Product:
 
     def __str__(self):
         return 'Номер товара:{} \tНазвание:{} \tЦена:{} \tКоличество:{}'.format(self.__id, self.__name, self.__price, self.__stock)
+
+class CartItem:
+    def __init__(self, product, qty):
+        self.__qty = qty
+        self.__product = product
+
+    @property
+    def product(self):
+        return self.__product
+   
+    @property
+    def qty(self):
+        return self.__qty
+
+    @qty.setter
+    def qty(self, qty):
+        if qty > 0:
+            self.__qty = qty
+        else:
+            print('Недопустимое значение')
+
+class Cart:
+    cartItems = []
+
+    def addProduct(self, product):
+        newItem = CartItem(product, 1)
+        self.cartItems.append(newItem)
+
+    def remove(self, productId):
+        foundCartItem = next((i for i in self.cartItems if i.product.id == productId), None)
+        self.cartItems.remove(foundCartItem)
+
+    def changeQty(self, productId, newQty):
+        foundCartItem = next((i for i in self.cartItems if i.product.id == productId), None)
+        foundCartItem.qty = newQty
+    
+    def geTotalPrice(self):
+        total = 0
+        for cartItem in self.cartItems:
+            total = (total + cartItem.product.price) * cartItem.qty
+        return total
+
     
 t = Product(1, 'Лейс', 100, 500)
 t2 = Product(2, 'Твикс', 35, 10)
 t3 = Product(3, 'Сникерс', 42, 53)
-products = [t, t2, t3]
 
-for p in products:
-    print(p)
+c = Cart()
 
-class Cart:
-    products = []
+c.addProduct(t)
+c.addProduct(t2)
+c.addProduct(t3)
 
-    def add(self, id: int, name: str, price: int, stock: int):
-        return products.append()
+c.remove(2)
+
+c.changeQty(1, 5)
+
+total = c.geTotalPrice()
+
+print()
+
+#products = [t, t2, t3]
+
+
+
+# for p in products:
+#     print(p)
+
+# class Cart:
+#     products = []
+
+#     def add(self, id: int, name: str, price: int, stock: int):
+
+#         return products.append()
 
 
 
